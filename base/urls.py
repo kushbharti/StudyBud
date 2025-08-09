d069f7ad57e606e7779a31b90fae1e2475915eb1
kushbharti <try.kushbharti@gmail.com>
2025-08-09 23:54:18 +0530

    templates override

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
+    path('',views.home,name='home'),
+    path('index/<int:pk>',views.index,name='index')
 ]
--- base/views.py
+++ base/views.py
@@ -3,5 +3,25 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms =[
+   {'id':1,'name':"hello"},
+   {'id':2,'name':'hii'},
+   {'id':3,'name':'guys'}
+]
+
+my_dict={
+   'name':'kush',
+   'age':19,
+   'class':'S.E-IT'
+}
+
 def home(request):
-   return render(request,'home.html')
+   return render(request,'home.html',{'rooms':rooms,'dict':my_dict})
+
+def index(request,pk):
+   room = None
+   for i in rooms:
+      if i['id'] == int(pk):
+         room =i
+         
+   return render(request,'index.html',{'rooms':room})
