7ed3108bf20c3762e483983f389ff15b1bb78d36
kushbharti <try.kushbharti@gmail.com>
2025-08-17 10:21:27 +0530

    fixing

--- base/views.py
+++ base/views.py
@@ -1,27 +1,24 @@
 from django.shortcuts import render
-
+from .models import Room
 
 # Create your views here.
 
-rooms =[
-   {'id':1,'name':"hello"},
-   {'id':2,'name':'hii'},
-   {'id':3,'name':'guys'}
-]
+# rooms =[
+#    {'id':1,'name':"hello"},
+#    {'id':2,'name':'hii'},
+#    {'id':3,'name':'guys'}
+# ]
 
-my_dict={
-   'name':'kush',
-   'age':19,
-   'class':'S.E-IT'
-}
+# my_dict={
+#    'name':'kush',
+#    'age':19,
+#    'class':'S.E-IT'
+# }
 
 def home(request):
-   return render(request,'home.html',{'rooms':rooms,'dict':my_dict})
+   rooms = Room.objects.all()
+   return render(request,'base/home.html',{'rooms':rooms})
 
 def index(request,pk):
-   room = None
-   for i in rooms:
-      if i['id'] == int(pk):
-         room =i
-         
-   return render(request,'index.html',{'rooms':room})
+   room = Room.objects.get(id=pk)
+   return render(request,'base/index.html',{'rooms':room})
